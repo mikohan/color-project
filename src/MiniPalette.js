3efddb15e89e9a9b723e8d6d1061c080f262f7ec
mikohan <angara99@gmail.com>
2020-09-29 15:19:19 +0300

    Add link to pallete

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -41,7 +41,7 @@ const styles = {
 };
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji, colors } = props;
+  const { classes, paletteName, emoji, colors, handleClick } = props;
   const miniColorBoxes = colors.map((color) => (
     <div
       className={classes.miniColor}
@@ -51,7 +51,7 @@ function MiniPalette(props) {
   ));
 
   return (
-    <div className={classes.root}>
+    <div className={classes.root} onClick={handleClick}>
       <div className={classes.colors}>{miniColorBoxes}</div>
       <h5 className={classes.title}>
         {paletteName} <span className={classes.emoji}>{emoji}</span>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -33,6 +33,13 @@ const styles = {
 };
 
 class PaletteList extends Component {
+  constructor(props) {
+    super();
+    this.goToPalette = this.goToPalette.bind(this);
+  }
+  goToPalette(id) {
+    this.props.history.push(`/palette/${id}`);
+  }
   render() {
     const { palettes, classes } = this.props;
     return (
@@ -43,7 +50,10 @@ class PaletteList extends Component {
           </nav>
           <div className={classes.palettes}>
             {palettes.map((palette) => (
-              <MiniPalette {...palette} />
+              <MiniPalette
+                {...palette}
+                handleClick={() => this.goToPalette(palette.id)}
+              />
             ))}
           </div>
         </div>
