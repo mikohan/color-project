9fe8195a1af5a7c9061f3e432084db6693efa061
mikohan <angara99@gmail.com>
2020-09-29 12:40:04 +0300

    Adeed router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColors from './seedColors';
 
@@ -7,9 +8,17 @@ import { generatePalette } from './colorhelper';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => <h1>Pallete list is working</h1>} />
+        <Route
+          exact
+          path="/palette/:id"
+          render={() => <h1>Pallete list is working</h1>}
+        />
+      </Switch>
+      // <div className="App">
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
