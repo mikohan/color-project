87cf87e4789c2a8ff9005b121be7225807b28616
mikohan <angara99@gmail.com>
2020-09-30 13:53:28 +0300

    Refactor css of ColorBox id finished

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -8,6 +8,17 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.scss';
 
 const styles = {
+  boxContent: {
+    position: 'absolute',
+    width: '100%',
+    left: '0px',
+    bottom: '0px',
+    padding: '10px',
+    color: 'black',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
+  },
   colorBox: {
     width: '20%',
     height: (props) => (props.showFullPalette ? '25%' : '50%'),
@@ -64,6 +75,56 @@ const styles = {
     textDecoration: 'none',
     opacity: '0',
   },
+  copyOverlay: {
+    opacity: '0',
+    zIndex: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.6s ease-in-out',
+    transform: 'scale(0.1)',
+  },
+  showOverlay: {
+    opacity: '1',
+    transform: 'scale(50)',
+    zIndex: '10',
+    position: 'absolute',
+  },
+  copyMessage: {
+    position: 'fixed',
+    left: '0',
+    right: '0',
+    top: '0',
+    bottom: '0',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '4rem',
+    transform: 'scale(0.1)',
+    opacity: '0',
+    color: '#fff',
+    '& h1': {
+      fontWeight: '400',
+      textShadow: '1px 2px rgba(225, 225, 225, 0.5)',
+      background: 'rgba(225, 225, 225, 0.3)',
+      width: '100%',
+      textAlign: 'center',
+      marginBottom: '0',
+      padding: '1rem',
+      textTransform: 'uppercase',
+    },
+    '& p': {
+      fontSize: '2rem',
+      fontWeight: '100',
+    },
+  },
+  showMessage: {
+    opacity: '1',
+    transform: 'scale(1)',
+    zIndex: '25',
+    transition: 'all 0.4s ease-in-out',
+    transitionDelay: '0.3s',
+  },
 };
 
 class ColorBox extends Component {
@@ -87,21 +148,25 @@ class ColorBox extends Component {
       classes,
     } = this.props;
     const copied = this.state.copied;
-    // const isDarkColor = chroma(background).luminance() <= 0.2;
-    //const isLightColor = chroma(background).luminance() >= 0.7;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
         <div style={{ background: background }} className={classes.colorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && 'show'}`}
+            className={`${classes.copyOverlay} ${
+              copied && classes.showOverlay
+            }`}
           />
-          <div className={`copy-msg ${copied && 'show'}`}>
+          <div
+            className={`${classes.copyMessage} ${
+              copied && classes.showMessage
+            }`}
+          >
             <h1>copied!</h1>
             <p className={classes.copyText}>{background}</p>
           </div>
-          <div className="copy-container">
-            <div className="box-content">
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyButton}>COPY</button>
