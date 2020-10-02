f0f5722c78bba7c6093312e4861ac5885bc80343
mikohan <angara99@gmail.com>
2020-10-02 13:07:22 +0300

    Make palette list responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes';
+
 export const styles = {
   root: {
     backgroundColor: 'blue',
@@ -7,11 +9,17 @@ export const styles = {
     justifyContent: 'center',
   },
   container: {
-    width: '50%',
+    width: '60%',
     display: 'flex',
     alignItems: 'flex-start',
     flexDirection: 'column',
     flexWrap: 'wrap',
+    [sizes.down('xl')]: {
+      width: '80%',
+    },
+    [sizes.down('md')]: {
+      width: '60%',
+    },
   },
   nav: {
     color: 'white',
@@ -28,6 +36,13 @@ export const styles = {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 30%)',
-    gridGap: '5%',
+    gridGap: '2.5rem',
+    [sizes.down('md')]: {
+      gridTemplateColumns: 'repeat(2, 50%)',
+    },
+    [sizes.down('xs')]: {
+      gridTemplateColumns: 'repeat(1, 100%)',
+      gridGap: '1.5rem',
+    },
   },
 };
