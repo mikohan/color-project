67aef736e4f3e451e367b0b54ffb5c577d3a7abc
mikohan <angara99@gmail.com>
2020-10-02 11:34:54 +0300

    Make colorbox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -26,11 +26,11 @@ export const styles = {
     },
     [sizes.down('lg')]: {
       width: '25%',
-      height: (props) => (props.showFullPalette ? '20%' : '50%'),
+      height: (props) => (props.showFullPalette ? '20%' : '33.3333%'),
     },
     [sizes.down('md')]: {
       width: '50%',
-      height: (props) => (props.showFullPalette ? '10%' : '50%'),
+      height: (props) => (props.showFullPalette ? '10%' : '20%'),
     },
     [sizes.down('xs')]: {
       width: '100%',
