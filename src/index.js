82fd077f1ae9d49867340e8e962ac56479df845b
mikohan <angara99@gmail.com>
2020-10-03 12:21:05 +0300

    Tried to use localisation no success by the way

--- src/index.js
+++ src/index.js
@@ -5,10 +5,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+import common_ru from './translations/ru/common.json';
+import common_en from './translations/en/common.json';
+
+i18next.init({
+  interpolation: { escapeValue: false }, // React already does escaping
+  lng: 'en', // language to use
+  resources: {
+    en: {
+      common: common_en, // 'common' is our custom namespace
+    },
+    ru: {
+      common: common_ru,
+    },
+  },
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <I18nextProvider i18n={i18next}>
+        <App />
+      </I18nextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
