55fca3730cf2f8ad0a556f74aa726c056e27f792
mikohan <angara99@gmail.com>
2020-10-13 11:00:14 +0300

    finished application

--- src/index.js
+++ src/index.js
@@ -11,25 +11,10 @@ import i18next from 'i18next';
 import common_ru from './translations/ru/common.json';
 import common_en from './translations/en/common.json';
 
-i18next.init({
-  interpolation: { escapeValue: false }, // React already does escaping
-  lng: 'en', // language to use
-  resources: {
-    en: {
-      common: common_en, // 'common' is our custom namespace
-    },
-    ru: {
-      common: common_ru,
-    },
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <I18nextProvider i18n={i18next}>
-        <App />
-      </I18nextProvider>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
